10807adb36a25f32df66b150ba485b47aab91b68
sarahsakordaniels <ssdaniels13@gmail.com>
2019-04-03 14:15:31 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		status_code = 200
+		erb :index
+	end
+	
+	get '/info' do
+		status_code = 200
+		erb :info
 	end
 end
